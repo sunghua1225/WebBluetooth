b78006c16a24a4f77de1fa592ec110f32b4f21b4
edgarnyhus <39582089+edgarnyhus@users.noreply.github.com>
2018-05-25 00:56:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,39 @@
-document.getElementById('arduinoButton').onclick = function (event) {
-  alert('onClick!');
-  if (navigator.usb) {
-    talkToArduino();
-  } else {
-    alert('WebUSB not supported.');
-  }
-};
+// document.getElementById('arduinoButton').onclick = function (event) {
+//      if (navigator.bluetooth) {
+//         talkToArduino();
+//     } else {
+//         alert('WebBluetooth not supported.');
+//     }
+// };
 
+document.getElementById('arduinoButton').onclick = function (event) {
+    talkToArduino();
+};
+/*
 navigator.usb.addEventListener('connect', event => {
-  // Add |event.device| to the UI.
-  document.getElementById('targetA').innerHTML = 'Device added: ' + event.device.name;
+    // Add |event.device| to the UI.
+    document.getElementById('targetA').innerHTML = 'Device added: ' + event.device.name;
 });
 
 navigator.usb.addEventListener('disconnect', event => {
-  // Remove |event.device| from the UI.
-  document.getElementById('targetA').innerHTML = 'Device removed: ' + event.device.name;
+    // Remove |event.device| from the UI.
+    document.getElementById('targetA').innerHTML = 'Device removed: ' + event.device.name;
 });
+*/
 
 async function talkToArduino() {
-  try {
-    navigator.bluetooth.requestDevice({
-      acceptAllDevices: true,
-      optionalServices: ['usb_charger']
-    })
-    .then(device => { 
-      document.getElementById('targetA').innerHTML = 'Received: ' + device.name;
-    })
-    .catch(error => { 
-      document.getElementById('targetB').innerHTML = "Error: " + error;
-    });
-  } catch (error) {
-    document.getElementById('targetB').innerHTML = error;
-  }
+    try {
+        navigator.bluetooth.requestDevice({
+            acceptAllDevices: true,
+            optionalServices: ['usb_charger']
+        })
+            .then(device => {
+            document.getElementById('targetA').innerHTML = 'Received: ' + device.name;
+        })
+        .catch(error => {
+            document.getElementById('targetB').innerHTML = "Error: " + error;
+        });
+    } catch (error) {
+        document.getElementById('targetB').innerHTML = error;
+    }
 }
