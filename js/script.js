a23b51b7b99969ad6104b3b547558e5510ff228c
edgarnyhus <39582089+edgarnyhus@users.noreply.github.com>
2018-09-19 10:03:19 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,13 +24,19 @@ navigator.usb.addEventListener('disconnect', event => {
 
 async function talkToArduino() {
     try {
-        navigator.bluetooth.requestDevice({
-            filters: [{ services: ['OurService'] }]
-        })
-        //navigator.bluetooth.requestDevice({
-        //  acceptAllDevices: true,
-        //  optionalServices: ['battery_service']
-        //})
+        let options = {
+          filters: [
+            {services: ['heart_rate']},
+            {services: [0x1802, 0x1803]},
+            {services: ['c48e6067-5295-48d3-8d5c-0395f61792b1']},
+            {name: 'ExampleName'},
+            {namePrefix: 'Prefix'}
+          ],
+          optionalServices: ['battery_service']
+          // OR: acceptAllDevices: true,
+        }
+        
+        navigator.bluetooth.requestDevice(options)
         .then(device => {
             document.getElementById('targetA').innerHTML = 'Received: ' + device.name;
         })
