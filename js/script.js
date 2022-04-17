8b52e5d8e5492bf9dd7a3b99772e78a3e3e90777
sunghua1225 <91591036+sunghua1225@users.noreply.github.com>
2022-04-17 16:17:07 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 
 async function connectDevice() {
+//     navigator.bluetooth.requestDevice({
+//       filters: [{
+//         services: ['00002A05-0000-1000-8000-00805F9B34FB']
+          
+          
+//           //00002A05-0000-1000-8000-00805F9B34FB
+//       }]
+//     })
+//     .then(device => { /* … */ })
+//     .catch(error => { console.error(error); });
+    
+    
     navigator.bluetooth.requestDevice({
       filters: [{
-        services: ['00002A05-0000-1000-8000-00805F9B34FB']
-          
-          
-          //00002A05-0000-1000-8000-00805F9B34FB
-      }]
+        name: 'Pay controller'
+      }],
+      optionalServices: ['battery_service'] // Required to access service later.
     })
     .then(device => { /* … */ })
     .catch(error => { console.error(error); });
-        
 }
 
