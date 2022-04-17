3322284ea0f0b16a2b38d78dffc4ba414416761d
sunghua1225 <91591036+sunghua1225@users.noreply.github.com>
2022-04-17 16:15:29 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,43 +1,11 @@
-async function talkToNRF() {
-    try {
-        //let options = {
-        //  filters: [
-        //    {services: ['heart_rate']},
-        //    {services: [0x1802, 0x1803]},
-        //    {services: ['c48e6067-5295-48d3-8d5c-0395f61792b1']},
-        //    {name: 'OurService'},
-        //    {namePrefix: 'Prefix'}
-        //  ],
-        //  optionalServices: ['battery_service']
-        //  // OR: acceptAllDevices: true,
-        //}
-        let options = {
-          filters: [
-            {services: [0x2902]},
-            {name: 'Pay controller'}
-          ]
-         }
-        
-        navigator.bluetooth.requestDevice({
-           acceptAllDevices: true,
-           optionalServices: ['battery_service']
-        })
-//         navigator.bluetooth.requestDevice(options)
-//         .then(device => {
-//             document.getElementById('targetA').innerHTML = 'Received: ' + device.name;
-//         })
-        .catch(error => {
-            document.getElementById('targetB').innerHTML = "Error: " + error;
-        });
-    } catch (error) {
-        document.getElementById('targetB').innerHTML = error;
-    }
-}
 
 async function connectDevice() {
     navigator.bluetooth.requestDevice({
       filters: [{
-        services: [0x1801, '00002A05-0000-1000-8000-00805F9B34FB']
+        services: ['00002A05-0000-1000-8000-00805F9B34FB']
+          
+          
+          //00002A05-0000-1000-8000-00805F9B34FB
       }]
     })
     .then(device => { /* … */ })
