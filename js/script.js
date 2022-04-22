bb6b9c63187dc91b583094e4ec1dece94443a550
sunghua1225 <91591036+sunghua1225@users.noreply.github.com>
2022-04-22 11:16:27 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,9 +14,9 @@ async function connectDevice() {
     
     navigator.bluetooth.requestDevice({
       filters: [{
-        name: 'Pay controller'
+        name: 'Pay'
       }],
-      optionalServices: ['battery_service'] // Required to access service later.
+      optionalServices: ['Pay controller'] // Required to access service later.
     })
     .then(device => { /* … */ })
     .catch(error => { console.error(error); });
