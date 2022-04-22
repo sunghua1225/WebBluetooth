c2fb6dc19b168f91b5a6b268f2764e8b671a4178
sunghua1225 <91591036+sunghua1225@users.noreply.github.com>
2022-04-22 14:44:59 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 
 async function connectDevice() {
     
-    //let dateNow=Date.now();
     
     navigator.bluetooth.requestDevice({
       filters: [{
@@ -9,24 +8,8 @@ async function connectDevice() {
       }],
       optionalServices: ['Pay controller'] // Required to access service later.
     })
-    .then( device => {
-          
-    }
-         )
+    .then(device => { /* … */ })
     .catch(error => { console.error(error); });
           
-          
-    //let data = {name: 'DelftStack'};
      
-//           fetch("https://gateway.biandianxia.com", {
-//             method: "POST", 
-//             body: JSON.stringify({
-//                       UUID: '003C120K0fEQDRR01',
-//                       device_id: '52312',
-//                       charge_id: dateNow,
-//                       minute: '1'
-//             })
-//           }).then(res => {
-//             characteristic.writeValue(res);
-//           });
 }
