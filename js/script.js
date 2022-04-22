9301d6f295633cc9d8e1fab453a1f76ea2156976
sunghua1225 <91591036+sunghua1225@users.noreply.github.com>
2022-04-22 13:25:59 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ async function connectDevice() {
 //     .then(device => { /* … */ })
 //     .catch(error => { console.error(error); });
     
+    let dateNow=Date.now();
     
     navigator.bluetooth.requestDevice({
       filters: [{
@@ -18,7 +19,45 @@ async function connectDevice() {
       }],
       optionalServices: ['Pay controller'] // Required to access service later.
     })
-    .then(device => { /* … */ })
+    .then(device => {
+          fetch("https://gateway.biandianxia.com", {
+            method: "POST", 
+            body: JSON.stringify({
+                      UUID: '003C120K0fEQDRR01',
+                      device_id: '52312',
+                      charge_id: dateNow,
+                      minute: '1'
+            })
+          }).then(res => {
+            characteristic.writeValue(res);
+          });
+    })
     .catch(error => { console.error(error); });
+          
+          
+    //let data = {name: 'DelftStack'};
+     
+          fetch("https://gateway.biandianxia.com", {
+            method: "POST", 
+            body: JSON.stringify({
+                      UUID: '003C120K0fEQDRR01',
+                      device_id: '52312',
+                      charge_id: dateNow,
+                      minute: '1'
+            })
+          }).then(res => {
+            characteristic.writeValue(res);
+          });
+}
+
+
+function postData(path, name, value){
+          document.getElementById("formField").name  = name;
+   	document.getElementById("formField").value = value;
+    
+    document.getElementById("form").action = path;
+    document.getElementById("form").submit();
+    
+    //postData("https://www.wikipedia.org/","Writer","Jim Collins");
 }
 
