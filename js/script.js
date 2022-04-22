97a747b887091efecbff475d2bdfb104efb644bb
sunghua1225 <91591036+sunghua1225@users.noreply.github.com>
2022-04-22 13:28:49 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
 
 async function connectDevice() {
-//     navigator.bluetooth.requestDevice({
-//       filters: [{
-//         services: ['00002A05-0000-1000-8000-00805F9B34FB']
-          
-          
-//           //00002A05-0000-1000-8000-00805F9B34FB
-//       }]
-//     })
-//     .then(device => { /* … */ })
-//     .catch(error => { console.error(error); });
     
     let dateNow=Date.now();
     
@@ -19,7 +9,7 @@ async function connectDevice() {
       }],
       optionalServices: ['Pay controller'] // Required to access service later.
     })
-    .then(device => {
+    .then( //device => {
           fetch("https://gateway.biandianxia.com", {
             method: "POST", 
             body: JSON.stringify({
@@ -31,33 +21,22 @@ async function connectDevice() {
           }).then(res => {
             characteristic.writeValue(res);
           });
-    })
+    //}
+         )
     .catch(error => { console.error(error); });
           
           
     //let data = {name: 'DelftStack'};
      
-          fetch("https://gateway.biandianxia.com", {
-            method: "POST", 
-            body: JSON.stringify({
-                      UUID: '003C120K0fEQDRR01',
-                      device_id: '52312',
-                      charge_id: dateNow,
-                      minute: '1'
-            })
-          }).then(res => {
-            characteristic.writeValue(res);
-          });
+//           fetch("https://gateway.biandianxia.com", {
+//             method: "POST", 
+//             body: JSON.stringify({
+//                       UUID: '003C120K0fEQDRR01',
+//                       device_id: '52312',
+//                       charge_id: dateNow,
+//                       minute: '1'
+//             })
+//           }).then(res => {
+//             characteristic.writeValue(res);
+//           });
 }
-
-
-function postData(path, name, value){
-          document.getElementById("formField").name  = name;
-   	document.getElementById("formField").value = value;
-    
-    document.getElementById("form").action = path;
-    document.getElementById("form").submit();
-    
-    //postData("https://www.wikipedia.org/","Writer","Jim Collins");
-}
-
