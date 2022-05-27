4c545fd6ccfa3ac5ee1363325dc36dd151832ac1
sunghua1225 <91591036+sunghua1225@users.noreply.github.com>
2022-05-27 10:09:24 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 let button = document.getElementById("connect");
 
+let options = {
+  filters: [
+    {services: ['heart_rate']},
+    {services: [0x1802, 0x1803]},
+    {services: ['c48e6067-5295-48d3-8d5c-0395f61792b1']},
+    {name: 'ExampleName'},
+    {namePrefix: 'Prefix'}
+  ],
+  optionalServices: ['battery_service']
+}
+
+
 button.addEventListener("click", async () => { 
-    navigator.bluetooth.requestDevice({
-//       filters: [{
-//         name: 'Pay'
-//       }],
-        acceptAllDevices: true,
-      optionalServices: ['Pay controller'] // Required to access service later.
+    navigator.bluetooth.requestDevice(options).then(function(device) {
+      console.log('Name: ' + device.name);
+      // Do something with the device.
     })
-    .then(device => { /* … */ })
-    .catch(error => { console.error(error); });
-  });
+    .catch(function(error) {
+      console.log("Something went wrong. " + error);
+    });
+});
 
